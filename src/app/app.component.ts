56be3e7011e2b06b3b6533c857114d662734cbde
younggugchoo <30790629+younggugchoo@users.noreply.github.com>
2018-01-24 15:18:25 +0900

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidebarMenu } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'scm-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scm works!!!';
+
+  currentMenu:SidebarMenu;
 }
